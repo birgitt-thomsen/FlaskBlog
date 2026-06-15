dd13a3a8d06a054aaa2f0a0bdca0c6d5decb7bed
Birgitt Thomsen <birgitt.thomsen@t-online.de>
2026-06-15 14:28:10 +0200

    create index route

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+""" This module handles Flask app initialization and routes. """
+
+import json
+from flask import Flask, render_template
+
+FILE_NAME = 'data/data.json'
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    """ Loads the json data file and displays it via the index page. """
+    with open(FILE_NAME, encoding='utf-8') as json_file:
+        blog_posts = json.load(json_file)
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
+    # app.run(host="0.0.0.0", port=5000, debug=True) # returns Not Found
